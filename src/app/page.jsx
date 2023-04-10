3fb1f17eb14d2747d1117234276fde91a6cf50f4
hshanz <david00gbg@hotmail.com>
2023-04-10 17:24:05 +0200

    Done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ import { useEffect, useRef, useState } from "react";
 export default function Home() {
   const [images, setImages] = useState([{}]);
   const [text, setText] = useState("Once upon a time there was a cat going to a castle, to find food. The cat was happy but very hungry, but the prospect of food made the cat very excited for the future.");
-  const [tone, setTone] = useState('')
+  const [tone, setTone] = useState('Optimistic and hopeful with a bit of anticipation and something')
   const [emotions, setEmotions] = useState({
     Happy: "#ffdab9",
     Sad: "#add8e6",
@@ -17,36 +17,35 @@ export default function Home() {
   
 
   useEffect(() => {
-    // const dalleImage = async (inputprompt) => {
-    //   const data = await generateImage(inputprompt);
-    //   setImages(data.data);
-    //   console.log('New image')
-    // };
+    const dalleImage = async (inputprompt) => {
+      const data = await generateImage(inputprompt);
+      setImages(data.data);
+      console.log('New image')
+    };
 
-    // const getToneEmotion = async(message) =>{
-    //   const data = await getEmotions(message);
-    //   setEmotions(data.Emotions)
-    //   setTone(data.Tone)
-    //   console.log('New emotions and tone')
-    // }
+    const getToneEmotion = async(message) =>{
+      const data = await getEmotions(message);
+      setEmotions(data.Emotions)
+      setTone(data.Tone)
+      console.log('New emotions and tone')
+    }
 
-    // const getPrompt = async (story) =>{
-    //   const prompt = await createPrompt(story)
-    //   dalleImage(prompt)
+    const getPrompt = async (story) =>{
+      const prompt = await createPrompt(story)
+      dalleImage(prompt)
 
-    // }
+    }
 
-    // const interval = setInterval(() => {
-    //  getToneEmotion(textArea.current.value)
-    //  getPrompt(textArea.current.value)
-    //  console.log('Done')
+    const interval = setInterval(() => {
+     getToneEmotion(textArea.current.value)
+     getPrompt(textArea.current.value)
+     console.log('Done')
 
-    // }, 100000000000000);
+    }, 90000);
 
-
-    // return () => clearInterval(interval);
-
-    
+    return () => clearInterval(interval);
+    dalleImage('Evening, Forest, Lost, Old House and Rain, abstract')
+    console.log('start')
 
   }, []);
 
@@ -58,7 +57,14 @@ export default function Home() {
 
       <div className={styles.contentHolder}>
         <div className={styles.textHolder}>
-
+        <div className={styles.inspoTags}> 
+          <div className={styles.tag} style={{'backgroundColor':'rgb(107, 107, 107)', 'color':"white"}}>Inspiration:</div>
+          <div className={styles.tag}>Evening</div>
+          <div className={styles.tag}>Forest</div>
+          <div className={styles.tag}>Lost</div>
+          <div className={styles.tag}>Old House</div>
+          <div className={styles.tag}>Rain</div>
+        </div>
 
           <textarea
             className={styles.textBox}
@@ -68,6 +74,12 @@ export default function Home() {
         </div>
 
         <div className={styles.pictureBox}>
+        <div className={styles.emotionHolder}>
+          <div className={styles.emotionHeader}>Emotions</div>
+          {Object.entries(emotions).map((e,index) => {return (<div key={index} className={styles.emotionBox} style={{ 'backgroundColor':e[1]}}>{e[0]}</div>)})}
+          
+        </div>
+
           <img
             src={images[0]?.url}
             alt="cat"
@@ -76,11 +88,16 @@ export default function Home() {
           
         
 
+        
+
         <div className={styles.emotionHolder}>
-          <div className={styles.emotionHeader}>Emotions</div>
-          {Object.entries(emotions).map(e => {return (<div className={styles.emotionBox} style={{ 'backgroundColor':e[1]}}>{e[0]}</div>)})}
-          <div className={styles.emotionHeader}>Tone</div>
+        <span className={styles.emotionHeader}> Tone </span>
+          <div className={styles.emotionHeader}>{tone}</div>
+          
         </div>
+
+        
+
         </div>
 
 
